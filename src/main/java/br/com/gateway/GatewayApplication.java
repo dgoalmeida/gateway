6409e123d2f326e001c07aca263602a11c45372e
Diego Almeida <diegoalmeida@MacBook-Pro-de-Diego.local>
2023-10-23 23:53:14 +0100

    test

--- src/main/java/br/com/gateway/GatewayApplication.java
+++ src/main/java/br/com/gateway/GatewayApplication.java
@@ -1,11 +1,13 @@
 package br.com.gateway;
 
+import io.opentelemetry.instrumentation.spring.autoconfigure.EnableOpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableOpenTelemetry
 public class GatewayApplication {
 
 	public static void main(String[] args) {
